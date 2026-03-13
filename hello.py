72456bc19d886c591bb1cbeaf906edb27460cdad
Siam Ashraf Anan <siamanan7@gmail.com>
2026-03-13 23:27:15 +0600

    Day 6: first Python project

---
+++ hello.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+print("Hello " + name + "! You are " + age + " years old.")
+print("You are going to Wellington in 2026. Keep going.")
